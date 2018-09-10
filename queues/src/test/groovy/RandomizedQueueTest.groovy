a40da2a887478cd523fd360d5887ade660608b71
Marcin A. Nowak <ian@sonyx.net>
2018-09-10 14:46:29 +0200

    tests run, command line runner in, library import in

---
+++ queues/src/test/groovy/RandomizedQueueTest.groovy
@@ -0,0 +1,79 @@
+import spock.lang.Shared
+import spock.lang.Specification
+
+class RandomizedQueueTest extends Specification {
+    @Shared
+    def rq = new RandomizedQueue()
+
+    def "initially size zero"() {
+        expect:
+        rq.size() == 0
+    }
+
+    def "initially empty"() {
+        expect:
+        rq.isEmpty() == true;
+    }
+
+    def "enqueue null arg"() {
+        when:
+        rq.enqueue(null)
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "failRemoveFirstIfEmpty"() {
+        given:
+        def rq2 = new MyDeque()
+        when:
+        rq2.removeFirst()
+        then:
+        thrown(NoSuchElementException)
+    }
+
+    def "failRemoveLastIfEmpty"() {
+        given:
+        def rq2 = new MyDeque()
+        when:
+        rq2.removeLast()
+        then:
+        thrown(NoSuchElementException)
+    }
+
+    def "failNextIfLast"() {
+        given:
+        def i1 = rq.iterator()
+        when:
+        i1.next()
+        then:
+        thrown(NoSuchElementException)
+    }
+
+    def "failIteratorRemove"() {
+        given:
+        def i1 = rq.iterator()
+        when:
+        i1.remove()
+        then:
+        thrown(UnsupportedOperationException)
+    }
+
+    def "queueElement"() {
+        given:
+        def rq2 = new RandomizedQueue<Integer>()
+        rq2.enqueue(new Integer(1))
+        expect:
+        rq2.size() == 1
+    }
+
+    def "queueThreeElements"() {
+        given:
+        def rq2 = new RandomizedQueue<Integer>()
+        rq2.enqueue(new Integer(1))
+        rq2.enqueue(new Integer(2))
+        rq2.enqueue(new Integer(3))
+        expect:
+        rq2.size() == 3
+    }
+
+}
