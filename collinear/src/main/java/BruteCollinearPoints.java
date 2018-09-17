64872771a237c81a032ee87bab83e30e3c4c3158
Marcin A. Nowak <ian@sonyx.net>
2018-09-17 11:23:39 +0200

    bruteCollinearPoints should be good for now

--- collinear/src/main/java/BruteCollinearPoints.java
+++ collinear/src/main/java/BruteCollinearPoints.java
@@ -1,17 +1,19 @@
+import java.util.ArrayList;
+
 public class BruteCollinearPoints {
   Point[] points;
 
   public BruteCollinearPoints(Point[] points)    // finds all line segments containing 4 points
   {
-    if( points == null){
+    if (points == null) {
       throw new IllegalArgumentException();
     }
-    for( int i = 0; i < points.length; i++){
-      for( int j=0; j < points.length; j++ ){
-        if( points[i] == null || points[j] == null){
+    for (int i = 0; i < points.length; i++) {
+      for (int j = 0; j < points.length; j++) {
+        if (points[i] == null || points[j] == null) {
           throw new IllegalArgumentException();
         }
-        if( i!=j && points[i].compareTo(points[j]) == 0){
+        if (i != j && points[i].compareTo(points[j]) == 0) {
           throw new IllegalArgumentException();
         }
       }
@@ -21,10 +23,78 @@ public class BruteCollinearPoints {
 
   public int numberOfSegments()        // the number of line segments
   {
-    return this.points.length;
+    int segmentsCount = 0;
+
+    for( int i=0; i<this.points.length;i++){
+      for( int j=i; j<this.points.length;j++){
+        if( i==j ){
+          continue;
+        }
+        for( int k=j; k<this.points.length;k++){
+          if( k==i || k ==j ){
+            continue;
+          }
+          for( int l=k; l<this.points.length;l++){
+            if( l==k|| l==j || l==i){
+              continue;
+            }
+            if( areCollinear(this.points[i],
+                this.points[j],this.points[k],this.points[l])){
+              segmentsCount++;
+            }
+          }
+        }
+      }
+    }
+    return segmentsCount;
   }
 
   public LineSegment[] segments() {
-    return null;
+    final LineSegment[] segments = new LineSegment[numberOfSegments()];
+    int segmentIndex = 0;
+
+    for( int i=0; i<this.points.length;i++){
+      for( int j=i; j<this.points.length;j++){
+        if( i==j ){
+          continue;
+        }
+        for( int k=j; k<this.points.length;k++){
+          if( k==i || k ==j ){
+            continue;
+          }
+          for( int l=k; l<this.points.length;l++){
+            if( l==k|| l==j || l==i){
+              continue;
+            }
+            if( areCollinear(this.points[i],
+                this.points[j],this.points[k],this.points[l])){
+              segments[segmentIndex++] = new LineSegment(this.points[i],this.points[l]);
+            }
+          }
+        }
+      }
+    }
+    return segments;
   }
+  private boolean areCollinear( Point p1, Point p2, Point p3, Point p4){
+    final double slope1 = p1.slopeTo(p2);
+    final double slope2 = p2.slopeTo(p3);
+    final double slope3 = p3.slopeTo(p4);
+    if( slope1 == slope2 && slope2 == slope3 ){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  /*
+  private Segments[] pointSegments( int pointIndex ){
+    final LineSegment[] segments = new LineSegment[this.points.length-1];
+    for( int i=0; i<this.points.length;i++){
+      segments[i] = new LineSegment( )
+    }
+
+  }
+  */
 }
--- collinear/src/test/groovy/BruteCollinearPointsTest.groovy
+++ collinear/src/test/groovy/BruteCollinearPointsTest.groovy
@@ -30,8 +30,41 @@ class BruteCollinearPointsTest extends Specification {
 
 
     def "NumberOfSegments"() {
+        given:
+        def p1 = new Point(0,0)
+        def p2 = new Point(1,1)
+        def p3 = new Point(2,2)
+        def p4 = new Point(3,3)
+        def Point[] ps1 = [p1,p2,p3,p4]
+        when:
+        def bcp = new BruteCollinearPoints(ps1)
+        then:
+        bcp.segments().size()==1
     }
 
-    def "Segments"() {
+    def "SegmentsCount1"() {
+        given:
+        def p1 = new Point(0,0)
+        def p2 = new Point(1,1)
+        def p3 = new Point(2,2)
+        def p4 = new Point(3,3)
+        def Point[] ps1 = [p1,p2,p3,p4]
+        when:
+        def bcp = new BruteCollinearPoints(ps1)
+        then:
+        bcp.numberOfSegments() == 1
     }
+    def "SegmentsCount0"() {
+        given:
+        def p1 = new Point(0,1)
+        def p2 = new Point(1,1)
+        def p3 = new Point(2,2)
+        def p4 = new Point(3,3)
+        def Point[] ps1 = [p1,p2,p3,p4]
+        when:
+        def bcp = new BruteCollinearPoints(ps1)
+        then:
+        bcp.numberOfSegments() == 0
+    }
+    // not sure how to test segments now...
 }
