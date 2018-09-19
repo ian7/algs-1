2dab6a92b16d47fe912817840cc95d976dacdcd8
Marcin A. Nowak <ian@sonyx.net>
2018-09-19 14:12:43 +0200

    solver works!

--- puzzle/src/main/java/Solver.java
+++ puzzle/src/main/java/Solver.java
@@ -1,17 +1,82 @@
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solver {
+  private final Node initial;
+  private MinPQ<Node> queue;
+
   public Solver(Board initial) {
+    this.initial = new Node(initial,0);
+    this.queue = new MinPQ<>();
+    queue.insert(this.initial);
 
   }          // find a solution to the initial board (using the A* algorithm)
+
   public boolean isSolvable() {
+
     return false;
   }           // is the initial board solvable?
+
   public int moves() {
     return 0;
   }                     // min number of moves to solve initial board; -1 if unsolvable
+
   public Iterable<Board> solution() {
-    return null;
+    List<Board> solution = new ArrayList<>();
+    Board predecessor = null;
+
+    while (!this.queue.min().isGoal()) {
+      Node min = this.queue.delMin();
+      solution.add(min.board);
+      for (Node neighbor : min.neighbors()) {
+        // first optimization
+        if( predecessor == null || (
+            predecessor != null && !predecessor.equals(neighbor.board))) {
+          this.queue.insert(neighbor);
+        }
+      }
+      predecessor = min.board;
+    }
+    solution.add(this.queue.min().board);
+    return solution;
   }     // sequence of boards in a shortest solution; null if unsolvable
+
   public static void main(String[] args) {
 
   } // solve a slider puzzle (given below)
+
+  private class Node implements Comparable<Node> {
+    private Board board;
+    private int depth;
+
+    Node(Board b, int depth) {
+      this.board = b;
+      this.depth = depth;
+    }
+
+    @Override
+    public int compareTo(Node that) {
+      return Integer.compare(this.metric(),that.metric());
+    }
+
+    private int metric() {
+      return this.depth + this.board.hamming();
+    }
+
+    public boolean isGoal(){
+      return this.board.isGoal();
+    }
+
+    public List<Node> neighbors(){
+      List<Node> neighbors = new ArrayList<>();
+      for( Board board : this.board.neighbors()){
+        neighbors.add( new Node( board, this.depth+1 ));
+      }
+      return neighbors;
+    }
+
+  }
+
 }
--- puzzle/src/test/groovy/SolverTest.groovy
+++ puzzle/src/test/groovy/SolverTest.groovy
@@ -1,4 +1,13 @@
 import spock.lang.Specification
 
 class SolverTest extends Specification {
+    def "legit construction"(){
+        when:
+        def blocks = [[0,1,3],[4,2,5],[7,8,6]] as int[][]
+        def b = new Board(blocks)
+        def s = new Solver(b)
+        def solution = s.solution()
+        then:
+        1==1
+    }
 }
