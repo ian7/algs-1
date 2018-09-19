88d5a24f228224235beb4ebe5e03cfe7dc0bbe66
Marcin A. Nowak <ian@sonyx.net>
2018-09-19 09:38:40 +0200

    goal testing works now

--- puzzle/src/main/java/Board.java
+++ puzzle/src/main/java/Board.java
@@ -1,6 +1,33 @@
 public class Board {
-  public Board(int[][] blocks) {
+  private int[][] blocks;
+  private int size;
+  private int priorMovesCount;
 
+  public Board(int[][] blocks) {
+    // let's do assertions and copy the stuff locally
+    this.size = blocks.length;
+    this.blocks = blocks.clone();
+    boolean checked[] = new boolean[this.size * this.size];
+
+    // checking if array is rectangular
+    if (blocks.length != blocks[0].length) {
+      throw new IllegalArgumentException();
+    }
+
+    for (int i = 0; i < this.size; i++) {
+      for (int j = 0; j < this.size; j++) {
+        if (blocks[i][j] < 0 || blocks[i][j] > this.size * this.size) {
+          throw new IllegalArgumentException();
+        }
+        final int blockValue = blocks[i][j];
+
+        if (checked[blockValue]) {
+          throw new IllegalArgumentException();
+        } else {
+          checked[blockValue] = true;
+        }
+      }
+    }
   }           // construct a board from an n-by-n array of blocks
 
   // (where blocks[i][j] = block in row i, column j)
@@ -17,7 +44,21 @@ public class Board {
   }                 // sum of Manhattan distances between blocks and goal
 
   public boolean isGoal() {
-    return false;
+    for (int i = 0; i < this.size; i++) {
+      for (int j = 0; j < this.size; j++) {
+        final int blockIndex = i * this.size + j;
+        final int expectedValue;
+        if (blockIndex < this.size * this.size-1) {
+          expectedValue = blockIndex + 1;
+        } else {
+          expectedValue = 0;
+        }
+        if (this.blocks[i][j] != expectedValue) {
+          return false;
+        }
+      }
+    }
+    return true;
   }                // is this board the goal board?
 
   public Board twin() {
--- puzzle/src/test/groovy/BoardTest.groovy
+++ puzzle/src/test/groovy/BoardTest.groovy
@@ -1,4 +1,62 @@
+import spock.lang.Shared
 import spock.lang.Specification
 
 class BoardTest extends Specification {
+    @Shared
+    def blocks = [[0,1,3],[4,2,5],[7,6,8]] as int[][]
+
+    def "legit construction"(){
+        when:
+        def b = new Board(blocks)
+        then:
+        1==1
+    }
+
+    def "non-rectangular blocks"(){
+        when:
+        blocks = new int[3][5]
+        new Board(blocks)
+        then:
+        thrown( IllegalArgumentException )
+    }
+
+    def "elements out of range-"(){
+        when:
+        blocks[1][1]=-1
+        new Board(blocks)
+        then:
+        thrown( IllegalArgumentException )
+    }
+
+    def "elements out of range+"(){
+        when:
+        blocks[1][1]=124
+        new Board(blocks)
+        then:
+        thrown( IllegalArgumentException )
+    }
+
+    def "duplicate elements"(){
+        when:
+        blocks[0][0]=1
+        blocks[0][1]=1
+        new Board(blocks)
+        then:
+        thrown( IllegalArgumentException )
+    }
+
+    def "isGoalNegative"(){
+        when:
+        def b = new Board(blocks)
+        then:
+        b.isGoal() == false
+    }
+
+    def "isGoalPositive"(){
+        when:
+        blocks = [[1,2,3],[4,5,6],[7,8,0]] as int[][]
+        def b = new Board(blocks)
+        then:
+        b.isGoal() == true
+    }
 }
