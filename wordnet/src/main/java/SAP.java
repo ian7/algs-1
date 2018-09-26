130e13cfe7dac047e09119de9a0e6684febe6000
Marcin A. Nowak <ian@sonyx.net>
2018-09-26 13:38:40 +0200

    SAP completed

--- wordnet/src/main/java/SAP.java
+++ wordnet/src/main/java/SAP.java
@@ -1,4 +1,5 @@
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.Stack;
 
@@ -14,40 +15,54 @@ public class SAP {
 
   // length of shortest ancestral path between v and w; -1 if no such path
   public int length(int v, int w){
-    return 0;
+    final Queue<Integer> queueLeft = new Queue<>();
+    final Queue<Integer> queueRight = new Queue<>();
+    queueLeft.enqueue(v);
+    queueRight.enqueue(w);
+    return this.length(queueLeft,queueRight);
   }
 
   // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
   public int ancestor(int v, int w){
-    final Queue<Integer> left = new Queue<>();
-    final Stack<Integer> right = new Stack<>();
+    final Queue<Integer> queueLeft = new Queue<>();
+    final Queue<Integer> queueRight = new Queue<>();
+    queueLeft.enqueue(v);
+    queueRight.enqueue(w);
+    return this.ancestor(queueLeft,queueRight);
+  }
 
-    boolean done = false;
 
+
+  // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+  public int length(Iterable<Integer> v, Iterable<Integer> w) {
     Queue<Integer> queueLeft = new Queue<>();
-    for( Integer i : this.digraph.adj(v)){
+    for (Integer i : v) {
       queueLeft.enqueue(i);
     }
+
     Queue<Integer> queueRight = new Queue<>();
-    for( Integer i : this.digraph.adj(w)){
+    for (Integer i : w) {
       queueRight.enqueue(i);
     }
 
     final HashMap<Integer,Integer> distancesLeft = new HashMap<>();
     final HashMap<Integer,Integer> distancesRight = new HashMap<>();
 
-    int currentDistance = 1;
+    int currentDistance = 0;
     int ancestor = -1;
+    int length = -1;
 
     while(ancestor==-1){
       Queue<Integer> nextLeftRow = new Queue<>();
       Queue<Integer> nextRightRow = new Queue<>();
+
       for( Integer i : queueLeft ){
         if( !distancesLeft.containsKey( i )) {
           distancesLeft.put(i, currentDistance);
         }
         if( distancesRight.containsKey(i)){
           ancestor = i;
+          length = distancesRight.get(i) + distancesLeft.get(i);
           break;
         }
         for( Integer j : this.digraph.adj(i)){
@@ -60,6 +75,7 @@ public class SAP {
         }
         if( distancesLeft.containsKey(i)){
           ancestor = i;
+          length = distancesRight.get(i) + distancesLeft.get(i);
           break;
         }
         for( Integer j : this.digraph.adj(i)){
@@ -73,22 +89,66 @@ public class SAP {
         return -1;
       }
     }
-
-    return ancestor;
-  }
-
-
-
-  // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-  public int length(Iterable<Integer> v, Iterable<Integer> w){
-    return 0;
-
+    return length;
   }
 
   // a common ancestor that participates in shortest ancestral path; -1 if no such path
   public int ancestor(Iterable<Integer> v, Iterable<Integer> w){
-    return 0;
+    Queue<Integer> queueLeft = new Queue<>();
+    for (Integer i : v) {
+      queueLeft.enqueue(i);
+    }
 
+    Queue<Integer> queueRight = new Queue<>();
+    for (Integer i : w) {
+      queueRight.enqueue(i);
+    }
+
+    final HashMap<Integer,Integer> distancesLeft = new HashMap<>();
+    final HashMap<Integer,Integer> distancesRight = new HashMap<>();
+
+    int currentDistance = 0;
+    int ancestor = -1;
+    int length = -1;
+
+    while(ancestor==-1){
+      Queue<Integer> nextLeftRow = new Queue<>();
+      Queue<Integer> nextRightRow = new Queue<>();
+
+      for( Integer i : queueLeft ){
+        if( !distancesLeft.containsKey( i )) {
+          distancesLeft.put(i, currentDistance);
+        }
+        if( distancesRight.containsKey(i)){
+          ancestor = i;
+          length = distancesRight.get(i) + distancesLeft.get(i);
+          break;
+        }
+        for( Integer j : this.digraph.adj(i)){
+          nextLeftRow.enqueue(j);
+        }
+      }
+      for( Integer i : queueRight ){
+        if( !distancesRight.containsKey( i )) {
+          distancesRight.put(i, currentDistance);
+        }
+        if( distancesLeft.containsKey(i)){
+          ancestor = i;
+          length = distancesRight.get(i) + distancesLeft.get(i);
+          break;
+        }
+        for( Integer j : this.digraph.adj(i)){
+          nextRightRow.enqueue(j);
+        }
+      }
+      queueLeft = nextLeftRow;
+      queueRight = nextRightRow;
+      currentDistance++;
+      if( queueLeft.size() == 0 && queueRight.size() == 0 && ancestor == -1){
+        return -1;
+      }
+    }
+    return ancestor;
   }
 
   // do unit testing of this class
--- wordnet/src/test/groovy/SAPTest.groovy
+++ wordnet/src/test/groovy/SAPTest.groovy
@@ -1,39 +1,64 @@
 import edu.princeton.cs.algs4.Digraph
+import spock.lang.Shared
 import spock.lang.Specification
 
 class SAPTest extends Specification {
-    def "construction"(){
-        given:
+    @Shared
+            sap
+
+    def setup() {
         def g = new Digraph(25)
-        g.addEdge(13,7)
-        g.addEdge(14,7)
-        g.addEdge(7,3)
-        g.addEdge(3,1)
-        g.addEdge(1,0)
-        g.addEdge(2,0)
-        g.addEdge(8,3)
-        g.addEdge(9,3)
-        g.addEdge(4,1)
-        g.addEdge(6,2)
-        g.addEdge(5,2)
-        g.addEdge(10,5)
-        g.addEdge(17,10)
-        g.addEdge(18,10)
-        g.addEdge(11,5)
-        g.addEdge(12,5)
-        g.addEdge(19,12)
-        g.addEdge(20,12)
-        g.addEdge(24,20)
-        g.addEdge(23,20)
-        g.addEdge(15,9)
-        g.addEdge(16,9)
-        g.addEdge(21,16)
-        g.addEdge(22,16)
-        def sap = new SAP(g)
-        expect:
-        sap.ancestor(13,2) == 0
-        sap.ancestor(13,4) == 1
-        sap.ancestor(13,16) == 3
-        sap.ancestor(17,24) == 5
+        g.addEdge(13, 7)
+        g.addEdge(14, 7)
+        g.addEdge(7, 3)
+        g.addEdge(3, 1)
+        g.addEdge(1, 0)
+        g.addEdge(2, 0)
+        g.addEdge(8, 3)
+        g.addEdge(9, 3)
+        g.addEdge(4, 1)
+        g.addEdge(6, 2)
+        g.addEdge(5, 2)
+        g.addEdge(10, 5)
+        g.addEdge(17, 10)
+        g.addEdge(18, 10)
+        g.addEdge(11, 5)
+        g.addEdge(12, 5)
+        g.addEdge(19, 12)
+        g.addEdge(20, 12)
+        g.addEdge(24, 20)
+        g.addEdge(23, 20)
+        g.addEdge(15, 9)
+        g.addEdge(16, 9)
+        g.addEdge(21, 16)
+        g.addEdge(22, 16)
+        sap = new SAP(g)
     }
+
+    def "individual ancestors"() {
+        expect:
+        sap.ancestor(13, 2) == 0
+        sap.ancestor(13, 4) == 1
+        sap.ancestor(13, 16) == 3
+        sap.ancestor(17, 24) == 5
+    }
+    def "individual length"() {
+        expect:
+        sap.length(13, 16) == 4
+    }
+    def "any ancestor"() {
+        given:
+        def left = [13,23,24]
+        def right = [6,16,17]
+        expect:
+        sap.ancestor(left, right) == 3
+    }
+    def "any length"() {
+        given:
+        def left = [13,23,24]
+        def right = [6,16,17]
+        expect:
+        sap.length(left, right) == 4
+    }
+
 }
