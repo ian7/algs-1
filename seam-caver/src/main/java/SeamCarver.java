e8768acafa4738e118cfc07c2f4f87194a0db77f
Marcin A. Nowak <ian@sonyx.net>
2018-10-03 11:20:12 +0200

    gradient and engery calculations seem to be right

--- seam-caver/src/main/java/SeamCarver.java
+++ seam-caver/src/main/java/SeamCarver.java
@@ -1,6 +1,8 @@
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.Picture;
 
+import java.awt.*;
+
 public class SeamCarver {
   private final Picture picture;
   Digraph digraph;
@@ -24,8 +26,33 @@ public class SeamCarver {
     return picture.height();
   }                          // height of current picture
   public  double energy(int x, int y)  {
-    return 0;
-  }              // energy of pixel at column x and row y
+    if( 0 == y || this.height()-1 == y ||
+        0 == x || this.width()-1 == x ){
+      return 1000;
+    }
+    return Math.sqrt(gradientX(x,y)+gradientY(x,y));
+  }
+
+  private double gradientX(int x, int y){
+    final Color left = this.picture.get(x-1,y);
+    final Color right = this.picture.get(x+1,y);
+    return gradient(left, right);
+  }
+
+  private double gradientY(int x, int y){
+    final Color top = this.picture.get(x,y-1);
+    final Color bottom = this.picture.get(x,y+1);
+    return gradient(top, bottom);
+  }
+
+  private double gradient(Color before, Color after) {
+    final double r = after.getRed() - before.getRed();
+    final double g = after.getGreen() - before.getGreen();
+    final double b = after.getBlue() - before.getBlue();
+    return r * r + g * g + b * b;
+  }
+
+  // energy of pixel at column x and row y
   public   int[] findHorizontalSeam() {
     return null;
   }              // sequence of indices for horizontal seam
--- seam-caver/src/test/groovy/SeamCarverTest.groovy
+++ seam-caver/src/test/groovy/SeamCarverTest.groovy
@@ -11,15 +11,56 @@ class SeamCarverTest extends Specification {
     def "faulty removeHorizontalSeam"(){
         when:
         def sc = new SeamCarver(new Picture(3,3))
-        sc.removeHorizontalSeam(null);
+        sc.removeHorizontalSeam(null)
         then:
         thrown IllegalArgumentException
     }
     def "faulty removeVerticalSeam"(){
         when:
         def sc = new SeamCarver(new Picture(3,3))
-        sc.removeVerticalSeam(null);
+        sc.removeVerticalSeam(null)
         then:
         thrown IllegalArgumentException
+    }
+    def "load from file 3x4.png"(){
+        when:
+        def sc = new SeamCarver(new Picture("3x4.png"))
+        then:
+        sc.width()==3
+        sc.height()==4
+    }
+    def "edge gradient 1000"(){
+        when:
+        def sc = new SeamCarver(new Picture("3x4.png"))
+        then:
+        sc.energy(0,0) == 1000d
+        sc.energy(0,1) == 1000d
+        sc.energy(0,2) == 1000d
+        sc.energy(0,3) == 1000d
+        sc.energy(2,0) == 1000d
+        sc.energy(2,1) == 1000d
+        sc.energy(2,2) == 1000d
+        sc.energy(2,3) == 1000d
+        sc.energy(1,0) == 1000d
+        sc.energy(1,3) == 1000d
+    }
+    def "gradientX"(){
+        when:
+        def sc = new SeamCarver(new Picture("3x4.png"))
+        then:
+        sc.gradientX(1,2) == 41620d
+    }
+    def "gradientY"(){
+        when:
+        def sc = new SeamCarver(new Picture("3x4.png"))
+        then:
+        sc.gradientY(1,2) == 10404d
+    }
+    def "non-trivial gradient"(){
+        when:
+        def sc = new SeamCarver(new Picture("3x4.png"))
+        then:
+        sc.energy(1,2) == Math.sqrt(52024d)
+        sc.energy(1,1) == Math.sqrt(52225d)
     }
 }
