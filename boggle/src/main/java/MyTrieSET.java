3bcb1f75dd5592fca8f161a0d0be88c4a8aafd96
Marcin A. Nowak <ian@sonyx.net>
2018-10-31 15:22:56 +0100

    depth first search and string tries with reduced radix did the job

--- boggle/src/main/java/MyTrieSET.java
+++ boggle/src/main/java/MyTrieSET.java
@@ -4,28 +4,28 @@ import edu.princeton.cs.algs4.Queue;
 import java.util.Iterator;
 
 /**
- *  The {@code TrieSET} class represents an ordered set of strings over
- *  the extended ASCII alphabet.
- *  It supports the usual <em>add</em>, <em>contains</em>, and <em>delete</em>
- *  methods. It also provides character-based methods for finding the string
- *  in the set that is the <em>longest prefix</em> of a given prefix,
- *  finding all strings in the set that <em>start with</em> a given prefix,
- *  and finding all strings in the set that <em>match</em> a given pattern.
- *  <p>
- *  This implementation uses a 256-way trie.
- *  The <em>add</em>, <em>contains</em>, <em>delete</em>, and
- *  <em>longest prefix</em> methods take time proportional to the length
- *  of the key (in the worst case). Construction takes constant time.
- *  <p>
- *  For additional documentation, see
- *  <a href="https://algs4.cs.princeton.edu/52trie">Section 5.2</a> of
- *  <i>Algorithms in Java, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * The {@code TrieSET} class represents an ordered set of strings over
+ * the extended ASCII alphabet.
+ * It supports the usual <em>add</em>, <em>contains</em>, and <em>delete</em>
+ * methods. It also provides character-based methods for finding the string
+ * in the set that is the <em>longest prefix</em> of a given prefix,
+ * finding all strings in the set that <em>start with</em> a given prefix,
+ * and finding all strings in the set that <em>match</em> a given pattern.
+ * <p>
+ * This implementation uses a 256-way trie.
+ * The <em>add</em>, <em>contains</em>, <em>delete</em>, and
+ * <em>longest prefix</em> methods take time proportional to the length
+ * of the key (in the worst case). Construction takes constant time.
+ * <p>
+ * For additional documentation, see
+ * <a href="https://algs4.cs.princeton.edu/52trie">Section 5.2</a> of
+ * <i>Algorithms in Java, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
  */
 public class MyTrieSET implements Iterable<String> {
-  private static final int R = 256;        // extended ASCII
+  private static final int R = 26;        // extended ASCII
 
   private Node root;      // root of trie
   private int n;          // number of keys in trie
@@ -67,7 +67,7 @@ public class MyTrieSET implements Iterable<String> {
     if (x == null) return null;
     if (d == key.length()) return x;
     char c = key.charAt(d);
-    return get(x.next[c], key, d + 1);
+    return get(x.next[c-'A'], key, d + 1);
   }
 
   /**
@@ -88,7 +88,7 @@ public class MyTrieSET implements Iterable<String> {
       x.isString = true;
     } else {
       char c = key.charAt(d);
-      x.next[c] = add(x.next[c], key, d + 1);
+      x.next[c-'A'] = add(x.next[c-'A'], key, d + 1);
     }
     return x;
   }
@@ -141,7 +141,7 @@ public class MyTrieSET implements Iterable<String> {
     if (x.isString) results.enqueue(prefix.toString());
     for (char c = 0; c < R; c++) {
       prefix.append(c);
-      collect(x.next[c], prefix, results);
+      collect(x.next[c-'A'], prefix, results);
       prefix.deleteCharAt(prefix.length() - 1);
     }
   }
@@ -172,12 +172,12 @@ public class MyTrieSET implements Iterable<String> {
     if (c == '.') {
       for (char ch = 0; ch < R; ch++) {
         prefix.append(ch);
-        collect(x.next[ch], prefix, pattern, results);
+        collect(x.next[ch-'A'], prefix, pattern, results);
         prefix.deleteCharAt(prefix.length() - 1);
       }
     } else {
       prefix.append(c);
-      collect(x.next[c], prefix, pattern, results);
+      collect(x.next[c-'A'], prefix, pattern, results);
       prefix.deleteCharAt(prefix.length() - 1);
     }
   }
@@ -207,7 +207,7 @@ public class MyTrieSET implements Iterable<String> {
     if (x.isString) length = d;
     if (d == query.length()) return length;
     char c = query.charAt(d);
-    return longestPrefixOf(x.next[c], query, d + 1, length);
+    return longestPrefixOf(x.next[c-'A'], query, d + 1, length);
   }
 
   /**
@@ -228,18 +228,17 @@ public class MyTrieSET implements Iterable<String> {
       x.isString = false;
     } else {
       char c = key.charAt(d);
-      x.next[c] = delete(x.next[c], key, d + 1);
+      x.next[c-'A'] = delete(x.next[c-'A'], key, d + 1);
     }
 
     // remove subtrie rooted at x if it is completely empty
     if (x.isString) return x;
     for (int c = 0; c < R; c++)
-      if (x.next[c] != null)
+      if (x.next[c-'A'] != null)
         return x;
     return null;
   }
 }
-
 
 
 /******************************************************************************
