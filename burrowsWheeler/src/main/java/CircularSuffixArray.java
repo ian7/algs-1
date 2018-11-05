e806edea43a6936c6743d27041fcbfdf07f1e3a6
Marcin Nowak <marcin.nowak@sonyx.net>
2018-11-05 10:54:08 +0100

    skeletons in

---
+++ burrowsWheeler/src/main/java/CircularSuffixArray.java
@@ -0,0 +1,13 @@
+public class CircularSuffixArray {
+    public CircularSuffixArray(String s) {
+
+    }   // circular suffix array of s
+    public int length(){
+        return 0;
+    }                     // length of s
+    public int index(int i){
+        return 0;
+    }                 // returns index of ith sorted suffix
+    public static void main(String[] args) {
+    } // unit testing (required)
+}
