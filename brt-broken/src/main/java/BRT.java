48ba8712e8b8d044cf6f4e7a49de6ac7e74857fc
Marcin Nowak <ian@sonyx.net>
2018-11-09 13:07:04 +0100

    adding kind of works

--- brt-broken/src/main/java/BRT.java
+++ brt-broken/src/main/java/BRT.java
@@ -1,23 +1,96 @@
-public class BRT<Key, Value> {
+public class BRT<Key extends Comparable<Key>, Value> {
+    Node<Key, Value> root;
+    int size;
 
-    private class Node<Value>{
+    BRT() {
+        this.root = null;
+    }
+
+    private class Node<Key, Value> {
         private Node left;
         private Node right;
-        Value v;
-        Key k;
-    }
-    public void add( Key k, Value v ){
+        private Value v;
+        private final Key k;
 
-    }
-    public boolean contains(Key k){
-        return false;
+        Node(Key k, Value v) {
+            this.k = k;
+            this.v = v;
+            this.left = null;
+            this.right = null;
+        }
+
+        public void setValue(Value v) {
+            this.v = v;
+        }
     }
 
-    public Value get(Key k){
-        return null;
+    public void add(Key k, Value v) {
+        Node<Key, Value> probe = this.find(k);
+        if (probe != null) {
+            probe.setValue(v);
+        } else {
+            Node<Key, Value> newOne = new Node(k, v);
+            if (this.root == null) {
+                this.root = newOne;
+            } else {
+                Node<Key, Value> pointer = this.root;
+                while (pointer != null) {
+                    if (pointer.k.compareTo(k) < 0) {
+                        if (pointer.left == null) {
+                            pointer.left = newOne;
+                            break;
+                        } else {
+                            pointer = pointer.left;
+                        }
+                    }
+                    if (pointer.k.compareTo(k) > 0) {
+                        pointer = pointer.right;
+                        if (pointer.right == null) {
+                            pointer.right = newOne;
+                            break;
+                        } else {
+                            pointer = pointer.right;
+                        }
+                    }
+                }
+            }
+            size++;
+        }
     }
 
-    public int size(){
-        return 0;
+    public boolean contains(Key k) {
+        return find(k) != null;
+    }
+
+    private Node find(Key k) {
+        Node<Key, Value> pointer = this.root;
+        while (pointer != null && pointer.k.compareTo(k) != 0) {
+            if (pointer.k.compareTo(k) < 0) {
+                pointer = pointer.left;
+                continue;
+            }
+            if (pointer.k.compareTo(k) > 0) {
+                pointer = pointer.right;
+                continue;
+            }
+        }
+        if (pointer == null) {
+            return null;
+        } else {
+            return pointer;
+        }
+    }
+
+    public Value get(Key k) {
+        Node<Key, Value> probe = this.find(k);
+        if (probe != null) {
+            return probe.v;
+        } else {
+            return null;
+        }
+    }
+
+    public int size() {
+        return this.size;
     }
 }
--- brt-broken/src/test/groovy/BRTTest.groovy
+++ brt-broken/src/test/groovy/BRTTest.groovy
@@ -7,4 +7,26 @@ class BRTTest extends Specification {
         then:
         brt.size() == 0
     }
+    def "adding one node"(){
+        given:
+        def brt = new BRT<Integer, String>()
+        when:
+        brt.add(1,"a");
+        then:
+        brt.size() == 1
+        brt.get(1) == "a"
+    }
+
+    def "adding three nodes"(){
+        given:
+        def brt = new BRT<Integer, String>()
+        brt.add(1,"a")
+        brt.add(2,"b")
+        brt.add(3,"c")
+        expect:
+        brt.size() == 3
+        brt.get(1)=="a"
+        brt.get(2)=="b"
+        brt.get(3)=="c"
+    }
 }
